5d921e1c16af97b74a5b21728213f0021b4c08e7
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-05-19 22:25:10 -0400

    initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+// CHALLENGE:
+// 1. Implement the add note functionality.
+// - Create a constant that keeps track of the title and content.
+// - Pass the new note back to the App.
+// - Add new note to an array.
+// - Take array and render seperate Note components for each item.
+
+// 2. Implement the delete note functionality.
+// - Callback from the Note component to trigger a delete function.
+// - Use the filter function to filter out the item that needs deletion.
+// - Pass a id over to the Note component, pass it back to the App when deleting.
+
+// This is the end result you're aiming for:
+// https://pogqj.csb.app/
+
+function App() {
+  // How to create a React app: npm create vite@latest {app-name} -- --template react
+
+
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
