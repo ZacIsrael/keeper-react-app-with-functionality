5af8e366cfbb40efad59dae066b1f18f8e22a132
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-05-21 09:40:47 -0400

    Used the HighlightIcon React component to display a highlighter icon in the header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,8 @@ import HighlightIcon from "@mui/icons-material/Highlight";
 function Header() {
   return (
     <header>
+      {/* adds a highlight icon/emoji  */}
+      <HighlightIcon />
       <h1>Keeper</h1>
     </header>
   );
